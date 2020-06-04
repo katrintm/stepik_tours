69560c38789ea1e845dd67c96f3b8693cd2003c7
katrintm <39545706+katrintm@users.noreply.github.com>
2020-06-04 09:25:31 +0300

    Update settings.py

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j24gbtrqa1)6$rdsde@s5xh)5(m9(*8(aea%@75*#bo1%tuf)m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -119,5 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-DIRECTOR = 'Ekaterina'
