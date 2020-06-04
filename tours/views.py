55fb5a3c43e6378fec8821e7589b0d272d20c528
tatya <tatya000@inbox.ru>
2020-06-04 09:01:35 +0300

    new release

---
+++ tours/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request, 'tours/index.html', context={
+
+            }
+        )
+
+
+class DepartureView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request, 'tours/departure.html', context={
+
+            }
+        )
+
+
+class TourView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request, 'tours/tour.html', context={
+
+            }
+        )
