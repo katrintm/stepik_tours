79a70e45f522ff82ed8316b8f85c7f2564025534
katrintm <39545706+katrintm@users.noreply.github.com>
2020-06-04 09:27:09 +0300

    Update urls.py

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from tours.views import MainView
 from tours.views import DepartureView
 from tours.views import TourView
+from tours.views import custom_handler404
 
 
 urlpatterns = [
@@ -9,3 +10,5 @@ urlpatterns = [
     path('departure/<str:departure>', DepartureView.as_view()),
     path('tour/<int:id>/', TourView.as_view()),
 ]
+
+handler404 = custom_handler404
